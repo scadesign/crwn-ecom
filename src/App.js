23304326e43bac1bc91dd6b1d820ed5bebe25751
scadesign <info@scadesign.co.uk>
2020-08-19 10:48:29 +0100

    update menu item to navigate with Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/hompage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats HomePage</h1>
+  </div>
+)
+
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+  <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path = '/hats' component={HatsPage} />
+    </Switch>
+  </div>
+  );
 }
 
 export default App;
