87a229178d905acf7f6d5d1ce9047af3d57c9272
scadesign <info@scadesign.co.uk>
2020-08-21 14:25:13 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -8,6 +10,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
   <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path = '/shop' component={ShopPage} />
